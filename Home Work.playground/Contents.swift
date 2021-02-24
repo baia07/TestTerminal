0c78415fdcb682391831d40e24d2a5e513d76019
Baiastan Abizhalilov <baiastanabizhalilov@MacBook-Air-Baiastan.local>
2021-02-24 22:22:19 +0600

    my Home Work

---
+++ Home Work.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+
+func power(a: Float , i : Float) -> Float {
+    return pow(a, i)
+}
+
+var x: Float = power(a: 5.0, i: 2.0)
+print(x)
---
+++ StringToArrayConverter.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+func converterFromStringToArray (s:String){
+    print(Array(s))
+    
+}
+
+converterFromStringToArray(s: "GeekTech")
